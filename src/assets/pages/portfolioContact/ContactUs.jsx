7c7a961d4b184e6b3cf2f6c7003e2140d95cb75b
Israel Oluniyi <israeloluniyi2004@gmail.com.com>
2022-10-22 16:25:38 +0100

    Chore: added new phone number

--- src/assets/pages/portfolioContact/ContactUs.jsx
+++ src/assets/pages/portfolioContact/ContactUs.jsx
@@ -16,7 +16,8 @@ export default class ContactUs extends Component {
                   <a href="mailto:israeloluniyi2004@gmail.com" className="lin">israeloluniyi2004@gmail.com</a>
                   <br/><br/>
                   <span className="Contactc">Phone</span><br/>
-                  <a href="tel:+2348069902175" className="lin">+2348069902175</a> <br/><br/>
+                  <a href="tel:+2348069902175" className="lin">+2348069902175</a> <br/> <div>or</div>
+                  <a href="tel:+2349025486481" className="lin">+2349025486481</a> <br/><br/>
                   &nbsp;<a href="mailto:israeloluniyi2004@gmail.com" className="linkt"> LET'S TALK</a>
               </div>
           </div>
