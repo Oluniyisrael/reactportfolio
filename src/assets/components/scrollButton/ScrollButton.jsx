5bd9d7a1944e2771f92afe11ec3306d6005b20df
Israel Oluniyi <israeloluniyi2004@gmail.com.com>
2022-11-09 17:21:28 +0100

    more stuffs on the button styling

--- src/assets/components/scrollButton/ScrollButton.jsx
+++ src/assets/components/scrollButton/ScrollButton.jsx
@@ -4,30 +4,28 @@ import "./ScrollButton.css"
 // import Null
 
 export default class ScrollButton extends Component {
-    constructor(props){
-      super(props)
+    constructor(){
+      super();
         this.state = {
           minDisappearHeight : "100px",
           showOnPx : "101px",
           backToTop : document.querySelectorAll('.backToTop'),
-          scrollContainer : () => {
-            return document.documentElement || document.body;
-          },
+          g : ()=> {this.scrollFunction()},
+          scrollFunction :()=> {
+            if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+                this.backToTop.style.display = "block";
+            } else {
+                this.backToTop.style.display = "none";
+                }
+            }
           
         };
     }
   render() {
     return (
       <div>
-        <button className='backToTop hidden'> 
-        {document.addEventListener("scroll", () => {
-  if (this.scrollContainer().scrollTop > this.showOnPx) {
-    this.backToTop.classList.remove("hidden")
-  } else {
-    this.backToTop.classList.add("hidden")
-  }
-})} back to top</button>
-        <button className='scrollButton'>  </button>
+        <button className='backToTop hidden' onScroll={this.g}> Back to top</button>
+        <button className='scrollButton'> Back to top </button>
       </div>
       
     )
