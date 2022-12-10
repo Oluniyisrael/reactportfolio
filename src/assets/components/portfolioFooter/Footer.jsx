a3599dbd2dadeca93f2ae38761b8340cef291739
Oluniyisrael <israeloluniyi2004@gmail.com>
2022-12-10 18:52:16 +0100

    Nothing

--- src/assets/components/portfolioFooter/Footer.jsx
+++ src/assets/components/portfolioFooter/Footer.jsx
@@ -8,6 +8,7 @@ export default class Footer extends Component {
         <div className="footer">
             <hr color="rgb(77, 89, 114)"/>
             <p className="footnote">© Copyright Oluniyi 2022 | Developed by <a href="https://facebook.com/oluniyiozy.israel" target='_blank' rel="noreferrer">Oluniyi Israel</a></p>
+             
         </div>
       </div>
     )
--- src/assets/components/scrollButton/ScrollButton.jsx
+++ src/assets/components/scrollButton/ScrollButton.jsx
@@ -4,17 +4,25 @@ import "./ScrollButton.css"
 function ScrollButton() {
   const [backToTop, setBackToTop] =useState(false);
   const top = ()=>{ window.scrollTo(0,0)};
+useEffect((
+  window.addEventListener("scroll",()=>{
+    if(window.scrolly > 3200){
+      setBackToTop (true)
+    }
+    else {setBackToTop(false)}
+  })
+),[])
 
-  useEffect(()=>{
-    window.addEventListener("scroll",()=>{
-      if (window.scrollY > 3200) {
-        setBackToTop(true)
-      }
-      else {
-        setBackToTop(false)
-      }
-    })
-  },[]);
+  // useEffect(()=>{
+  //   window.addEventListener("scroll",()=>{
+  //     if (window.scrollY > 3200) {
+  //       setBackToTop(true)
+  //     }
+  //     else {
+  //       setBackToTop(false)
+  //     }
+  //   })
+  // },[]);
   return (
     <div>
       {backToTop && <button className='scrollButton' onClick={top}>Back to top</button>
