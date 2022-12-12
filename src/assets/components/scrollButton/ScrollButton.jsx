e7f324a48e2b77e28318c36417f3862d75df170e
Oluniyisrael <israeloluniyi2004@gmail.com>
2022-12-12 09:17:24 +0100

    story for another time 12/12/2022

--- src/assets/components/scrollButton/ScrollButton.jsx
+++ src/assets/components/scrollButton/ScrollButton.jsx
@@ -1,24 +1,41 @@
-import React,{useState, useEffect} from 'react'
+import React
+// ,{useState, useEffect}
+ from 'react'
 import "./ScrollButton.css"
 
 function ScrollButton() {
-  const [backToTop, setBackToTop] =useState(false);
+  // const [backToTop, setBackToTop] =useState(false);
+  // const [scrollHeightV,setScrollHeightV]=useState('')
   const top = ()=>{ window.scrollTo(0,0)};
+  // var scrollHeightV = window.scrollY;
+  // var scrollDist = ;
+  // var mean = scrollHeightV;
+  // window.addEventListener("scroll",()=>{
+  //   setScrollHeightV(window.document.body.scrollHeight)
+  //   console.log(mean)
+  //   if(window.scrollY > 3200){
+  //     setBackToTop (true)
+  //   }
+  //   else {setBackToTop(false)
+  //   }
+  // 
+  // })
 
-  useEffect(()=>{
-    window.addEventListener("scroll",()=>{
-      if (window.scrollY > 3200) {
-        setBackToTop(true)
-      }
-      else {
-        setBackToTop(false)
-      }
-    })
-  },[]);
+  // useEffect(()=>{
+  //   window.addEventListener("scroll",()=>{
+  //     if (window.scrollY > 3200) {
+  //       setBackToTop(true)
+  //     }
+  //     else {
+  //       setBackToTop(false)
+  //     }
+  //   })
+  // },[]);
   return (
     <div>
-      {backToTop && <button className='scrollButton' onClick={top}>Back to top</button>
-      }
+      {/* {backToTop &&  */}
+      <button className='scrollButton' onClick={top}>Back to top</button>
+      {/* } */}
       
     </div>
   )
