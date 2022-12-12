d5f0f53f5c8cfc1f40d826d76a57d86efa9bf0ae
Oluniyisrael <israeloluniyi2004@gmail.com>
2022-12-12 18:07:16 +0100

    Done the dissapearing back to top button for js only

--- src/assets/components/scrollButton/ScrollButton.jsx
+++ src/assets/components/scrollButton/ScrollButton.jsx
@@ -1,41 +1,25 @@
-import React
-// ,{useState, useEffect}
+import React,{useState, useEffect}
  from 'react'
 import "./ScrollButton.css"
 
 function ScrollButton() {
-  // const [backToTop, setBackToTop] =useState(false);
-  // const [scrollHeightV,setScrollHeightV]=useState('')
+  const [backToTop, setBackToTop] =useState(false);
   const top = ()=>{ window.scrollTo(0,0)};
-  // var scrollHeightV = window.scrollY;
-  // var scrollDist = ;
-  // var mean = scrollHeightV;
-  // window.addEventListener("scroll",()=>{
-  //   setScrollHeightV(window.document.body.scrollHeight)
-  //   console.log(mean)
-  //   if(window.scrollY > 3200){
-  //     setBackToTop (true)
-  //   }
-  //   else {setBackToTop(false)
-  //   }
-  // 
-  // })
-
-  // useEffect(()=>{
-  //   window.addEventListener("scroll",()=>{
-  //     if (window.scrollY > 3200) {
-  //       setBackToTop(true)
-  //     }
-  //     else {
-  //       setBackToTop(false)
-  //     }
-  //   })
-  // },[]);
+  useEffect(() => {
+    window.addEventListener('scroll',()=>{
+      if (window.pageYOffset + window.innerHeight >= document.body.scrollHeight -100 ){
+        setBackToTop(true)
+      }
+      else{
+        setBackToTop(false)
+      }
+    }) 
+  }, [])
   return (
     <div>
-      {/* {backToTop &&  */}
+      {backToTop && 
       <button className='scrollButton' onClick={top}>Back to top</button>
-      {/* } */}
+       } 
       
     </div>
   )
