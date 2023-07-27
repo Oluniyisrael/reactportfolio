df0e98ff3fc90a41687e4051b5e3e336fd7602cc
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-07-27 18:08:31 +0100

    nye

--- src/assets/pages/portfolioWorks/Works.jsx
+++ src/assets/pages/portfolioWorks/Works.jsx
@@ -14,7 +14,8 @@ export default class Works extends Component {
                 <div className="wimg">
                     <div className="wimg1"> <a href="https://izzyrobofriends.netlify.app/"><img src={wimg1} alt="..." width="300px" height="200px"/> </a><p>Robofriends</p> </div>
                     <div className="wimg2"> <a href="https://topmostintc.netlify.app/"><img src={wimg2} alt="..." width="300px" height="200px"/></a> <p>Topmost International School</p></div>
-                    <div className="wimg3">  <a href="https://esdermablog.netlify.app/"><img src={wimg3} alt="..." width="300px" height="200px"/></a></div>
+                    <div className="wimg3">  <a href="https://esdermablog.netlify.app/"><img src={wimg3} alt="..." width="300px" height="200px"/></a>
+                    <p>Esderma Blog</p></div>
                 </div>
                 <hr width="60%" align="center"/>
                 <h2 className="needhelp">Need a place to learn a skill in tech?</h2>
